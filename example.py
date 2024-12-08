b68bccfe9ca9f8fe3f8c03b1389c282357b4092b
shahriar0999 <shahriar0999@proton.me>
2024-12-08 15:11:35 +0600

    add new branch

--- example.py
+++ example.py
@@ -2,6 +2,13 @@ def add(a, b):
     return a + b
 
 
+def mul(a, b):
+    return a * b
+
+
 print("This code give use addition of two number")
 
 print(add(3, 5))
+
+print("Result of Multiplications", mul(3, 2))
+
